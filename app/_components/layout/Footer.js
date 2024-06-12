d19e4c41ebd726e05b63947d80d6c984bddeccbd
matinsekhavat <matinhyphen@gmail.com>
2024-06-12 18:42:21 +0330

    create footer.js file

--- app/_components/layout/Footer.js
+++ app/_components/layout/Footer.js
@@ -1,7 +1,27 @@
-import style from "./Footer.module.css";
+import styles from "./Footer.module.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.desc}>
+        <h3>سامانه خرید و اجاره ملک</h3>
+        <p>
+          {" "}
+          در این پلتفرم شما قادر به خرید فروش رهن . اجاره ملاک زیبای هستید با
+          کمترین پورسانت و در این مجموعه کارمندان ما آماده کمک رسانی به شما
+          میباشند
+        </p>
+      </div>
+      <div>
+        <ul>
+          <li>تعرفه قانونی</li>
+          <li>مشاوره سریع</li>
+          <li>قیمت مناسب</li>
+          <li>کادر مجرب</li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
