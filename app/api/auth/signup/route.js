4664c31d584bf121abd7d3e678dec813ffffef91
matinsekhavat <matinhyphen@gmail.com>
2024-06-12 20:20:13 +0330

    fix some changes in signup file for catch body

--- app/api/auth/signup/route.js
+++ app/api/auth/signup/route.js
@@ -1,4 +1,4 @@
-const { NextResponse } = require("next/server");
+import { NextResponse } from "next/server";
 import userModel from "@/app/_models/User";
 import connectToDb from "@/app/_utils/connectToDb";
 import { hashPassword } from "@/app/_utils/auth";
@@ -6,8 +6,9 @@ import { hashPassword } from "@/app/_utils/auth";
 export async function POST(req) {
   try {
     await connectToDb();
-    const body = req.json();
+    const body = await req.json();
     const { email, password } = body;
+    console.log(email, password);
     //validate Data
     if (!email.trim() || !password.trim()) {
       NextResponse.json(
@@ -18,8 +19,11 @@ export async function POST(req) {
       );
     }
 
+    //convert email to lowercase
+    const lowerCaseEmail = email.toLowerCase();
+
     //check is user existed?
-    const isUserExisting = await userModel.findOne({ email });
+    const isUserExisting = await userModel.findOne({ email: lowerCaseEmail });
     if (isUserExisting) {
       return NextResponse.json(
         { error: "حساب کاربری با این ایمیل از قبل ایجاد شده است" },
@@ -27,19 +31,19 @@ export async function POST(req) {
       );
     }
 
-    const hashedpassword = await hashPassword(password);
+    const hashedPassword = await hashPassword(password);
 
     //create new user
     const newUser = await userModel.create({
-      email,
-      password: hashedpassword,
+      email: lowerCaseEmail,
+      password: hashedPassword,
     });
 
     return NextResponse.json({ message: "حساب شما با موفقیت ثبت نام شد" });
   } catch (error) {
     console.log(error);
     return NextResponse.json(
-      { error: "sth went wrong in server" },
+      { error: error.message },
       {
         status: 500,
       }
