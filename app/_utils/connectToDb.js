7239b3b6342c686c336d582b46f2cbdd367a620f
matinsekhavat <matinhyphen@gmail.com>
2024-06-12 20:18:41 +0330

    remove .env var in connectToDB

--- app/_utils/connectToDb.js
+++ app/_utils/connectToDb.js
@@ -6,7 +6,7 @@ export default async function connectToDb() {
       //true is once is already Connected
       return false;
     }
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect("mongodb://127.0.0.1/real-estate");
     console.log("Connected Successfully");
   } catch (error) {
     throw new Error("sth went wrong", error);
