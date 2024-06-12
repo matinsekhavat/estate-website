d3a842af1a76098d3ca94d5d28a2b0c8ad097014
matinsekhavat <matinhyphen@gmail.com>
2024-06-12 19:05:37 +0330

    create connectToDB utils file

---
+++ app/_utils/connectToDb.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export default async function connectToDb() {
+  try {
+    if (mongoose.connections[0].readyState) {
+      //true is once is already Connected
+      return false;
+    }
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected Successfully");
+  } catch (error) {
+    throw new Error("sth went wrong", error);
+  }
+}
