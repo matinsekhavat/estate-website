8e4bf3e7a0e29c195826c33357ffb1f563e12680
matinsekhavat <matinhyphen@gmail.com>
2024-06-12 19:40:57 +0330

    create POST function then handle catch area

--- app/api/auth/signup/route.js
+++ app/api/auth/signup/route.js
@@ -2,3 +2,16 @@ const { NextResponse } = require("next/server");
 import userModel from "@/app/_models/User";
 import connectToDb from "@/app/_utils/connectToDb";
 import { hashPassword } from "@/app/_utils/auth";
+
+export async function POST(req) {
+  try {
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "sth went wrong in server" },
+      {
+        status: 500,
+      }
+    );
+  }
+}
