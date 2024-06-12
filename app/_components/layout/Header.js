38a00f8f84c947db756d0f8c8fad0d2b8d4db441
matinsekhavat <matinhyphen@gmail.com>
2024-06-12 18:23:56 +0330

    for a while remove header file

---
+++ app/_components/layout/Header.js
@@ -0,0 +1,13 @@
+import { FiLogIn } from "react-icons/fi";
+import styles from "./Header.module.css";
+function Header() {
+  return (
+    <div>
+      <span>
+        <FiLogIn />
+      </span>
+    </div>
+  );
+}
+
+export default Header;
