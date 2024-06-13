66ac635264e9e913b222de47d8523a8cfab5bca1
matinsekhavat <matinhyphen@gmail.com>
2024-06-13 03:31:29 +0330

    Creating Signup page.js in template

---
+++ app/_components/template/SignupPage.js
@@ -0,0 +1,11 @@
+"use client";
+
+import { useState } from "react";
+
+function SignupPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return <div>SignupPage</div>;
+}
+
+export default SignupPage;
