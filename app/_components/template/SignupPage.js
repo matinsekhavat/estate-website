aac3f94cdc17c9cdd8b10229a0b7784a355b5ba6
matinsekhavat <matinhyphen@gmail.com>
2024-06-13 03:43:46 +0330

    create client SignUpPage.js component in template file

--- app/_components/template/SignupPage.js
+++ app/_components/template/SignupPage.js
@@ -1,11 +1,36 @@
 "use client";
-
 import { useState } from "react";
+import styles from "./SignupPage.module.css";
 
 function SignupPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  return <div>SignupPage</div>;
+  const [rePassword, setRePassword] = useState("");
+  return (
+    <div className={styles.form}>
+      <h4>فرم ثبت نام</h4>
+      <form action="">
+        <label> ایمیل</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />{" "}
+        <label> رمز عبور</label>
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />{" "}
+        <label> تکرار رمز عبور</label>
+        <input
+          type="text"
+          value={rePassword}
+          onChange={(e) => setRePassword(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default SignupPage;
