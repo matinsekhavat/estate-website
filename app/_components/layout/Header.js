ea033ea2c9fbf56efd8ebaacf58706096dd9786d
matinsekhavat <matinhyphen@gmail.com>
2024-06-14 17:05:18 +0330

    make useSession hook in header client components

--- app/_components/layout/Header.js
+++ app/_components/layout/Header.js
@@ -1,7 +1,13 @@
+"use client";
+
 import { FiLogIn } from "react-icons/fi";
 import styles from "./Header.module.css";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { FaUserAlt } from "react-icons/fa";
 function Header() {
+  const { data: session, status } = useSession();
+  console.log(session, status);
   return (
     <header className={styles.header}>
       <div>
@@ -14,12 +20,21 @@ function Header() {
           </li>
         </ul>
       </div>
-      <div className={styles.login}>
-        <Link href="/signin">
-          <FiLogIn />
-          <span>ورود</span>
-        </Link>
-      </div>
+
+      {!session ? (
+        <div className={styles.login}>
+          <Link href="/signin">
+            <FiLogIn />
+            <span>ورود</span>
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.login}>
+          <Link href="/dashboard">
+            <FaUserAlt />
+          </Link>
+        </div>
+      )}
     </header>
   );
 }
