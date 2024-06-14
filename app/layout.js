0092f7bf6f156475393e7eb1805f38aa91883624
matinsekhavat <matinhyphen@gmail.com>
2024-06-14 17:04:26 +0330

    wrapp nextSessionProvider into layout file

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Toaster } from "react-hot-toast";
 import AppLayout from "./_components/layout/AppLayout";
 import { yekanBakh } from "./_utils/fonts";
 import "./globals.css";
+import NextAuthProvider from "./_providers/NextAuthProvider";
 
 export const metadata = {
   title: "Create Next App",
@@ -12,8 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={yekanBakh.className}>
-        <Toaster />
-        {<AppLayout>{children}</AppLayout>}
+        <NextAuthProvider>
+          <Toaster />
+          {<AppLayout>{children}</AppLayout>}
+        </NextAuthProvider>
       </body>
     </html>
   );
