9df2740f4c0eca14c5f06fa51fe946b84af76ea6
matinsekhavat <matinhyphen@gmail.com>
2024-06-14 17:08:52 +0330

    handle signin page server to when user is loggin prevent to access to login page

--- app/(auth)/signin/page.js
+++ app/(auth)/signin/page.js
@@ -1,7 +1,11 @@
 import React from "react";
 import SigninPage from "./../../_components/template/SigninPage";
-
-function page() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+async function page() {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/");
   return <SigninPage />;
 }
 
