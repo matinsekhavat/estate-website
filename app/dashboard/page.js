ee66e438a698a01b3b1e182307a43fb26d164bd5
matinsekhavat <matinhyphen@gmail.com>
2024-06-15 18:44:07 +0330

    redirect unauth user into /signin page instead of / path

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -4,7 +4,7 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 async function page() {
   const session = await getServerSession(authOptions);
-  if (!session) redirect("/");
+  if (!session) redirect("/signin");
   return <div>dashboard page</div>;
 }
 
