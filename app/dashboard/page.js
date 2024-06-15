52b6ec07ee82d813b243dd1ac7b4c396deb9f395
matinsekhavat <matinhyphen@gmail.com>
2024-06-15 19:17:29 +0330

    implement find user and pass timeStamps into DashboardPage

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,10 +2,15 @@ import React from "react";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import DashboardPage from "../_components/template/DashboardPage";
+import userModel from "../_models/User";
 async function page() {
   const session = await getServerSession(authOptions);
+  const user = await userModel.findOne({ email: session?.user.email });
+  const timeStamps = user?.createdAt;
+
   if (!session) redirect("/signin");
-  return <div>dashboard page</div>;
+  return <DashboardPage timeStamps={timeStamps} />;
 }
 
 export default page;
