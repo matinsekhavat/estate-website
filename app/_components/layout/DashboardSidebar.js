d6c2fd49d53058c6765bab334e5c1fb0aca6d04d
matinsekhavat <matinhyphen@gmail.com>
2024-06-15 16:37:19 +0330

    add Logout components into DashboardSidebar

--- app/_components/layout/DashboardSidebar.js
+++ app/_components/layout/DashboardSidebar.js
@@ -5,6 +5,7 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import { CgProfile } from "react-icons/cg";
 import Link from "next/link";
+import LogoutButton from "../module/LogoutButton";
 async function DashboardSidebar({ children }) {
   const session = await getServerSession(authOptions);
   if (!session) redirect("/");
@@ -17,6 +18,7 @@ async function DashboardSidebar({ children }) {
         <Link href="/dashboard">حساب کاربری</Link>
         <Link href="/dashboardmy-profiles">آگهی های من</Link>
         <Link href="/dashboard/add">ثبت آگهی</Link>
+        <LogoutButton />
       </div>
       <div className={styles.main}>{children}</div>
     </div>
