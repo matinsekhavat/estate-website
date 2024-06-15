c52d6ed3f557aa56c505b1d8d623cc28bfbcd540
matinsekhavat <matinhyphen@gmail.com>
2024-06-15 19:16:03 +0330

    implement ui of DashboardPage.js

---
+++ app/_components/template/DashboardPage.js
@@ -0,0 +1,18 @@
+import styles from "./DashboardPage.module.css";
+
+function DashboardPage({ timeStamps }) {
+  console.log(timeStamps);
+
+  return (
+    <div className={styles.cotainer}>
+      <h3>سلام👋🏻</h3>
+      <p>آگهی های خود را ثبت کنید تا هزاران نفر آن را مشاهده کنند.</p>
+      <div className={styles.createdAt}>
+        <p>تاریخ عضویت:</p>
+        <span>x</span>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardPage;
