f9f39fee96125ca6b886bd1ef24f6ffdb80ccec4
matinsekhavat <matinhyphen@gmail.com>
2024-06-16 19:36:20 +0330

    include add profile page into main add path route

--- app/dashboard/add/page.js
+++ app/dashboard/add/page.js
@@ -1,7 +1,8 @@
+import AddProfilePage from "@/app/_components/template/AddProfilePage";
 import React from "react";
 
 function page() {
-  return <div>Add</div>;
+  return <AddProfilePage />;
 }
 
 export default page;
