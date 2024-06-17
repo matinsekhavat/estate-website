d4d263e94cc4b8e0172f88d76a814ec420f0f37a
matinsekhavat <matinhyphen@gmail.com>
2024-06-17 14:52:30 +0330

    create profile Models Schema

---
+++ app/_models/Profile.js
@@ -0,0 +1,57 @@
+import mongoose, { Schema } from "mongoose";
+
+const schema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    realState: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    constructionDate: {
+      type: Date,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ["villa", "apartment", "store", "office"],
+    },
+    amenities: {
+      type: [String],
+      default: [],
+    },
+    rules: {
+      type: [String],
+      default: [],
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const profileModel =
+  mongoose.models.Profile || mongoose.model("Profile", schema);
+
+export default profileModel;
