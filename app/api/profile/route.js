908c38a6f1273dbfd1188c2a495d48a7f987513e
matinsekhavat <matinhyphen@gmail.com>
2024-06-17 15:04:29 +0330

    destructure body data and then check user is login or not v-1.0

--- app/api/profile/route.js
+++ app/api/profile/route.js
@@ -1,6 +1,30 @@
+import connectToDb from "@/app/_utils/connectToDb";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
-
 export async function POST(req) {
   try {
-  } catch (error) {}
+    await connectToDb();
+    const body = await req.json();
+    const {
+      title,
+      description,
+      location,
+      phone,
+      realState,
+      price,
+      constructionDate,
+      category,
+      amenities,
+      rules,
+    } = await req.json();
+    const session = await getServerSession(req); // instead of authOptions we say req
+    if (!session) {
+      return NextResponse.json(
+        { error: "ابتدا وارد حساب خود شوید" },
+        { status: 401 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json({ error: "مشکلی در سرور هست" }, { status: 500 });
+  }
 }
