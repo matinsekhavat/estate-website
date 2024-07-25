d8124b3d352146ebb2a4151e07acf0d7e9fe8f44
matinsekhavat <matinhyphen@gmail.com>
2024-07-26 00:17:04 +0330

    implement and use demo ui of DashboardCard

--- app/_components/template/MyProfilesPage.js
+++ app/_components/template/MyProfilesPage.js
@@ -1,7 +1,15 @@
-import React from "react";
+import DashboadCard from "../module/DashboadCard";
 import styles from "./MyProfilesPage.module.css";
 function MyProfilesPage({ profiles }) {
-  return <div>MyProfilesPage</div>;
+  console.log(profiles);
+  return (
+    <div>
+      {!profiles.length && <p className={styles.text}>هیچ آکهی وجود ندارد</p>}
+      {profiles.map((profile) => (
+        <DashboadCard key={profile._id} profile={profile} />
+      ))}
+    </div>
+  );
 }
 
 export default MyProfilesPage;
