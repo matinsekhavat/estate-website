44926b5e70905649f79a00d7ef2f7feb8ca6ecd5
matinsekhavat <matinhyphen@gmail.com>
2024-07-26 15:10:54 +0330

    import Card.module.css file as style name into Card.js file

--- app/_components/module/Card.js
+++ app/_components/module/Card.js
@@ -1,8 +1,7 @@
-import React from "react";
-
+import styles from "./Card.module.css";
 function Card({ profile }) {
   const { category, location, price, title } = profile;
-  return <div>Card</div>;
+  return <div>{title}</div>;
 }
 
 export default Card;
