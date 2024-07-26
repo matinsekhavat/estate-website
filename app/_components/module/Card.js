91886fa2e24b8136e710cfa7db0c78e74a764a43
matinsekhavat <matinhyphen@gmail.com>
2024-07-26 15:17:12 +0330

    set Icons object for conet with category  in card.js file

--- app/_components/module/Card.js
+++ app/_components/module/Card.js
@@ -1,7 +1,21 @@
 import styles from "./Card.module.css";
+import { RiHome3Line } from "react-icons/ri";
+import { MdApartment } from "react-icons/md";
+import { BiStore } from "react-icons/bi";
+import { GiOfficeChair } from "react-icons/gi";
 function Card({ profile }) {
   const { category, location, price, title } = profile;
-  return <div>{title}</div>;
+  const icons = {
+    villa: <RiHome3Line />,
+    apartment: <MdApartment />,
+    store: <BiStore />,
+    office: <GiOfficeChair />,
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.icon}>{icons[category]}</div>
+    </div>
+  );
 }
 
 export default Card;
