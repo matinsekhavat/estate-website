c977924bd77f13da0aced8e55fea29b34b5baaf3
matinsekhavat <matinhyphen@gmail.com>
2024-07-28 19:44:42 +0330

    complete firsts ui schema of Card.js component

--- app/_components/module/Card.js
+++ app/_components/module/Card.js
@@ -1,8 +1,11 @@
 import styles from "./Card.module.css";
 import { RiHome3Line } from "react-icons/ri";
 import { MdApartment } from "react-icons/md";
-import { BiStore } from "react-icons/bi";
+import { BiLeftArrow, BiLeftArrowAlt, BiStore } from "react-icons/bi";
 import { GiOfficeChair } from "react-icons/gi";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { e2p, sp } from "@/app/_utils/replaceNumber";
+import Link from "next/link";
 function Card({ profile }) {
   const { category, location, price, title } = profile;
   const icons = {
@@ -14,6 +17,16 @@ function Card({ profile }) {
   return (
     <div className={styles.container}>
       <div className={styles.icon}>{icons[category]}</div>
+      <p className={styles.title}>{title}</p>
+      <p className={styles.location}>
+        <HiOutlineLocationMarker />
+        {location}
+      </p>
+      <span>{sp(price)} تومان </span>
+      <Link href="/">
+        مشاهده آگهی
+        <BiLeftArrowAlt />
+      </Link>
     </div>
   );
 }
