2ed9d00b6e73facefeba37d342acc40fc2b15a1e
matinsekhavat <matinhyphen@gmail.com>
2024-07-28 19:57:08 +0330

    add Dashboard Card Buttons

--- app/_components/module/DashboardCard.js
+++ app/_components/module/DashboardCard.js
@@ -1,10 +1,22 @@
 import React from "react";
 import styles from "./DashboardCard.module.css";
 import Card from "./Card";
+import { FiEdit } from "react-icons/fi";
+import { AiOutlineDelete } from "react-icons/ai";
 function DashboardCard({ profile }) {
   return (
     <div className={styles.container}>
       <Card profile={profile} />
+      <div className={styles.main}>
+        <button>
+          ویرایش
+          <FiEdit />
+        </button>
+        <button>
+          حذف آگهی
+          <AiOutlineDelete />
+        </button>
+      </div>
     </div>
   );
 }
