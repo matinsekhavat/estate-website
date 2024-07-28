083f1dc8e2a3cf5d5791e7aa244a92ee09a8bbf9
matinsekhavat <matinhyphen@gmail.com>
2024-07-28 19:52:07 +0330

    delete wrong file from DashboardCard.js file (wrong syntax)

--- app/_components/module/DashboardCard.js
+++ app/_components/module/DashboardCard.js
@@ -1,7 +1,12 @@
 import React from "react";
-
-function DashboardCard() {
-  return <div>DashboardCard</div>;
+import styles from "./DashboardCard.module.css";
+import Card from "./Card";
+function DashboardCard({ profile }) {
+  return (
+    <div className={styles.container}>
+      <Card profile={profile} />
+    </div>
+  );
 }
 
 export default DashboardCard;
