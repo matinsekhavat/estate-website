005873155ccc3e343296603a003b5d998610f523
matinsekhavat <matinhyphen@gmail.com>
2024-07-28 20:37:04 +0330

    develop profile - patch API

--- app/api/profile/route.js
+++ app/api/profile/route.js
@@ -75,3 +75,85 @@ export async function POST(req) {
     return NextResponse.json({ error: "مشکلی در سرور هست" }, { status: 500 });
   }
 }
+
+export async function PATCH(req) {
+  try {
+    const body = await req.json();
+    const {
+      title,
+      description,
+      location,
+      phone,
+      realState,
+      price,
+      constructionDate,
+      category,
+      amenities,
+      rules,
+      _id,
+    } = body;
+    // check user token
+    const session = await getServerSession(req); // instead of authOptions we say req
+    if (!session) {
+      return NextResponse.json(
+        { error: "ابتدا وارد حساب خود شوید" },
+        { status: 401 }
+      );
+    }
+    const user = await userModel.findOne({ email: session.user.email });
+
+    // check is user exist
+    if (!user) {
+      return NextResponse.json(
+        { error: "همچین کاربری یافت نشد" },
+        { status: 404 }
+      );
+    }
+    // valiate data
+    if (
+      !_id ||
+      !title ||
+      !location ||
+      !description ||
+      !phone ||
+      !realState ||
+      !price ||
+      !constructionDate ||
+      !category
+    ) {
+      return NextResponse.json(
+        { error: "لطفا اطلاعات معتبر وارد کنید" },
+        { status: 400 }
+      );
+    }
+
+    const profile = profileModel.findOne({
+      _id,
+    });
+    if (!user._id.equals(profile.userId)) {
+      return NextResponse.json(
+        {
+          error: "دسترسی شما به اگهی محدود است",
+        },
+        { status: 403 }
+      );
+    }
+    profile.title = title;
+    profile.description = description;
+    profile.location = location;
+    profile.price = price;
+    profile.phone = phone;
+    profile.realState = realState;
+    profile.constructionDate = constructionDate;
+    profile.rules = rules;
+    profile.amenities = amenities;
+    profile.category = category;
+    profile.save();
+    return NextResponse.json(
+      { message: "آگهی شما با موفقیت ثبت شد" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ error: "مشکلی در سرور هست" }, { status: 500 });
+  }
+}
