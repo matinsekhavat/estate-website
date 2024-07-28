d92091089c29153b7cbc70c906daa059023a4b68
matinsekhavat <matinhyphen@gmail.com>
2024-07-28 20:13:07 +0330

    make dashboardCard component ,  client component

--- app/_components/module/DashboardCard.js
+++ app/_components/module/DashboardCard.js
@@ -1,18 +1,21 @@
+"use client";
 import React from "react";
 import styles from "./DashboardCard.module.css";
 import Card from "./Card";
 import { FiEdit } from "react-icons/fi";
 import { AiOutlineDelete } from "react-icons/ai";
 function DashboardCard({ profile }) {
+  function editHandler() {}
+  function deleteHandler() {}
   return (
     <div className={styles.container}>
       <Card profile={profile} />
       <div className={styles.main}>
-        <button>
+        <button onClick={editHandler}>
           ویرایش
           <FiEdit />
         </button>
-        <button>
+        <button onClick={deleteHandler}>
           حذف آگهی
           <AiOutlineDelete />
         </button>
